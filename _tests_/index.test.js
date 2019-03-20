be863f24292bb94f39545b6a45e26d9cbd5124f5
pritishvaidya <pritishvaidya94@gmail.com>
2019-03-20 14:24:45 +0530

    Add sample tests :boom:

---
+++ _tests_/index.test.js
@@ -0,0 +1,9 @@
+import sampleRNHooks from '../src'
+
+describe('Test Template function', () => {
+  it('should be a function', () => {
+    expect(
+      typeof sampleRNHooks,
+    ).toBe('function');
+  });
+})
