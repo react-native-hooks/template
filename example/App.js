d1e40282b6d7162814ff1bbd75d569b882547b4f
pritishvaidya <pritishvaidya94@gmail.com>
2019-03-20 14:15:51 +0530

    Add example directory :star2:

---
+++ example/App.js
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import sampleRNHook from '@rnhooks/template'
+
+const instructionsText = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+function App () {
+  const [instructions] = useState(instructionsText)
+  let hook = sampleRNHook();
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Welcome to React Native!</Text>
+      <Text style={styles.instructions}>To get started, edit App.js</Text>
+      <Text style={styles.instructions}>{instructions}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+export default App
